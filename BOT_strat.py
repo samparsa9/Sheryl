f4d0659312965bbc37b4b3c0414420313649ecc8
malikkurtz <malikkurtz129@gmail.com>
2024-07-01 15:07:57 -0400

    Adding comments

--- BOT_strat.py
+++ BOT_strat.py
@@ -20,23 +20,34 @@ api = tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
 
 
 def main():
+    # When we run this algo for the very first time we won't be in a position
     in_position = False
-
+    # Specifying the location of where we want our sp500 csv to be loaded, or if it already exists, where it is to update it
     location_of_sp500_csv_file = 'Sheryl/sp500_companies.csv'
+    # Running wikapedia scraper to populate initial sp500 csv with ticker symbols of sp500 companies
     setup.Create_sp500_csv(location_of_sp500_csv_file)
+    # Setting our stockdf to be the csv file we just created
     stockdf = pd.read_csv('Sheryl/sp500_companies.csv', index_col='Symbol')
+    # Creating our tickers list which we will pass into the Calculate_features function
     tickers = setup.Create_list_of_tickers(stockdf.index)
+    # Calculating our features for each ticker and populating the dataframe with these values
     setup.Calculate_features(tickers, stockdf)
+    # Dropping any columns that may have resulted in NaN values
     stockdf = stockdf.dropna()
+    # Creating a scaled_data numpy array that we will pass into our K means algorithm
     scaled_data = setup.Scale_data(stockdf)
+    # Running k means
     setup.Apply_K_means(stockdf, scaled_data)
+    # Soring the data frame based on cluster value and saving these values to the dataframe, and then updating the csv
     setup.Sort_and_save(stockdf, location_of_sp500_csv_file)
+    # Creating a new dataframe that will contain information about the clusters that will be used for trading logic
     cluster_df = setup.cluster_df_setup(1000000, stockdf)
+    # Plotting cluster data
     setup.plot_clusters(stockdf)
     
 
 
-    while not True: # FOR DEBUGGING SET BACK TO TRUE
+    while not True: # FOR DEBUGGING, SET BACK TO TRUE
         try:
             print("----------New Clustering and Allocation Iteration---------")
             if not in_position and (datetime.now(timezone.utc).hour == 14 and datetime.now(timezone.utc).minute == 00):
