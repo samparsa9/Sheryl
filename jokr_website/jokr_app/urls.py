1282dd4185fe7019bd55039a5f08e8df786580ac
malikkurtz <malikkurtz129@gmail.com>
2024-07-08 23:54:04 -0400

    reorganization

--- jokr_website/jokr_app/urls.py
+++
@@ -1,8 +0,0 @@
-# jokr_app/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('data/', views.get_data, name='get_data'),
-]
