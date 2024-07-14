7f8f1f13947349b6c381f4a3913173aa35567daf
malikkurtz <malikkurtz129@gmail.com>
2024-07-14 14:14:49 -0400

    typo in model name

--- Financial Modeling/option_pricing_models.py
+++ Financial Modeling/option_pricing_models.py
@@ -67,8 +67,8 @@ r_list =          [0.0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1]
 
 option_prices = np.zeros((len(volatility_list), len(r_list)))
 
-binary_model = BinomialOptionPricingModel(100, 100, 1, 0.2, 0.05, 5, 'C')
-print(binary_model.calculate_derivative_price())
+binomial_model = BinomialOptionPricingModel(100, 100, 1, 0.2, 0.05, 5, 'C')
+print(binomial_model.calculate_derivative_price())
 # for i, sigma in enumerate(volatility_list):
 #     for j, r in enumerate(r_list):
 #         to_set = binomial_option_pricing_model(S0, K, T, sigma, r, N)
